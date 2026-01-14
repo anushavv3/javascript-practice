2b2fc2f8955ac7be5cc02474f06f7dd905a8b253
anushavv3 <anusha38vv@gmail.com>
2026-01-15 01:05:21 +0530

    add gb.js practice

---
+++ gb.js
@@ -0,0 +1,25 @@
+let bot;
+bot = "teacherBot";
+let botLocation = "the universe";
+bot = "professorBot";
+bot = "awesomeTeacherBot";
+const favoriteSubject = "Computer Science";
+
+console.log("Hi there!");
+console.log("I am excited to talk to you.");
+console.log("Allow me to introduce myself.");
+
+const botIntroduction = "My name is " + bot + ".";
+console.log(botIntroduction);
+
+const botLocationSentence = "I live in " + botLocation + ".";
+console.log(botLocationSentence);
+
+const nicknameIntroduction = "My nickname is " + bot + ".";
+console.log(nicknameIntroduction);
+
+const newNicknameGreeting = "I love my nickname but I wish people would call me " + bot + ".";
+console.log(newNicknameGreeting);
+
+const favoriteSubjectSentence = "My favorite subject is " + favoriteSubject + ".";
+console.log(favoriteSubjectSentence);
