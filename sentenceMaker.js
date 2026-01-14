2778b084c14c4e7b22340c054559c649bff5dc75
anushavv3 <anusha38vv@gmail.com>
2026-01-15 02:29:31 +0530

    add sentencemaker.js

--- sentenceMaker.js
+++ sentenceMaker.js
@@ -18,3 +18,4 @@ noun2="apples"
 
 let secondStory="Once upon a time, there was a(n)" +adjective+" " +noun+ " who loved to eat "+ noun2+". The "+ noun+ "lived in a " +place+" and had " +adjective2+" nostrils that blew fire when it was "+verb+".";
 console.log("Second Story:",secondStory);
+
