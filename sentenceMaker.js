43d317aefa39a92adee779f445346c7b60e4a79d
anushavv3 <anusha38vv@gmail.com>
2026-01-15 02:26:18 +0530

    add sentenceMaker..js

---
+++ sentenceMaker.js
@@ -0,0 +1,20 @@
+let adjective="magical";
+let noun="creature";
+let verb="roaring";
+let place="mountain";
+let adjective2="glowing";
+let noun2="strawberries";
+
+let firstStory="Once upon a time, there was a(n)" +adjective+" " +noun+ " who loved to eat "+ noun2+". The "+ noun+ "lived in a " +place+" and had " +adjective2+" nostrils that blew fire when it was "+verb+".";
+
+console.log("First Story: ",firstStory);
+
+adjective="fiery";
+noun="dragon"
+verb="flying"
+place='forest'
+adjective2="enormous"
+noun2="apples"
+
+let secondStory="Once upon a time, there was a(n)" +adjective+" " +noun+ " who loved to eat "+ noun2+". The "+ noun+ "lived in a " +place+" and had " +adjective2+" nostrils that blew fire when it was "+verb+".";
+console.log("Second Story:",secondStory);
