32816c2f55316050c3227e1387733378ca3ce271
anushavv3 <anusha38vv@gmail.com>
2026-01-15 21:47:27 +0530

    Fix spacing in code

--- sentenceMaker.js
+++ sentenceMaker.js
@@ -5,17 +5,18 @@ let place="mountain";
 let adjective2="glowing";
 let noun2="strawberries";
 
-let firstStory="Once upon a time, there was a(n)" +adjective+" " +noun+ " who loved to eat "+ noun2+". The "+ noun+ "lived in a " +place+" and had " +adjective2+" nostrils that blew fire when it was "+verb+".";
+let firstStory="Once upon a time, there was a(n) " +adjective+" "+noun+" who loved to eat "+noun2+". The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
 
-console.log("First Story: ",firstStory);
+console.log("First story: "+firstStory);
+
 
 adjective="fiery";
-noun="dragon"
-verb="flying"
-place='forest'
-adjective2="enormous"
-noun2="apples"
+noun="dragon";
+verb="flying";
+place='forest';
+adjective2="enormous";
+noun2="apples";
 
-let secondStory="Once upon a time, there was a(n)" +adjective+" " +noun+ " who loved to eat "+ noun2+". The "+ noun+ "lived in a " +place+" and had " +adjective2+" nostrils that blew fire when it was "+verb+".";
-console.log("Second Story:",secondStory);
+let secondStory="Once upon a time, there was a(n) " +adjective+" " +noun+ " who loved to eat "+ noun2+". The "+ noun+" lived in a " +place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
 
+console.log("Second story: "+secondStory);
