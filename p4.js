c5bc47b6a7fde910cdfce17915cf6b667021378a
anushavv3 <anusha38vv@gmail.com>
2026-01-17 22:05:05 +0530

    Add p4.js

---
+++ p4.js
@@ -0,0 +1,25 @@
+let age=null;
+console.log(typeof age);
+
+
+let g="hello";
+console.log(g[1]);
+console.log(g[g.length-1]);
+let fst= g[0]+g[1]+g[2];
+console.log(fst);
+
+
+
+let p="r ar r ,\nv are b,\njs is fun,\nso u a ";
+console.log(p);
+
+let s="She said, \"Hello!\"";
+console.log(s);
+
+let quote = 'It\'s a beautiful day!';
+console.log(quote); 
+
+const n="alice";
+const gr=`hello,${n}!`;
+console.log(gr);
+
