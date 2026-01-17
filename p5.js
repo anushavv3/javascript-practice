7ad85c7b91a469f1c1b0d003dd82f39459cae469
anushavv3 <anusha38vv@gmail.com>
2026-01-17 22:39:48 +0530

    Add p5.js

--- p5.js
+++ p5.js
@@ -16,3 +16,19 @@ const s=9.5;
 const hs=10;
 const o=`one of mt fav s is "${song}".I rated it ${(s/hs*100)}%`;
 console.log(o);
+
+
+let sentence="i am pretty";
+let position= sentence.indexOf("pretty");
+console.log(position);
+
+
+let sentences=" i got a job in jan 2026"
+let positions=sentences.indexOf("wow")
+console.log(positions);
+
+let sen="i am very happy and workhard i getting the result now ";
+let pos=sen.indexOf("i",15);
+console.log(pos);
+
+console.log("happy".indexOf('H'));
