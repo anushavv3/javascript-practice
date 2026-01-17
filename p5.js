b353c9a25f6d169ca5bc731156b481f5d623185b
anushavv3 <anusha38vv@gmail.com>
2026-01-17 22:05:56 +0530

    Add p5.js

---
+++ p5.js
@@ -0,0 +1,18 @@
+const n="alice";
+const a=30;
+const m=`my name is ${n} and i am ${a} years old`;
+console.log(m);
+
+
+let p=`r nsmN z
+hvsajdaj
+s mxa 
+xmznm
+aSNA`;
+console.log(p);
+
+const song="woh baarshien";
+const s=9.5;
+const hs=10;
+const o=`one of mt fav s is "${song}".I rated it ${(s/hs*100)}%`;
+console.log(o);
