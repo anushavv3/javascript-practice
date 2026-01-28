c9e8abb85981496c4d4e9017cc421b44b4b33082
anushavv3 <anusha38vv@gmail.com>
2026-01-29 00:32:41 +0530

    Day 1: variables, template literals, prompt and alert"

---
+++ day-01/day1.js
@@ -0,0 +1,13 @@
+let name="anusha";
+let age=23;
+let city="kannur";
+console.log("Hi , my name is "+name+", I am "+age+" years old, and I live in "+city+ ".");
+console.log(` Hi , my name is ${name}, I am ${age} years  old, and I live in ${city} .`);
+
+
+let userName= prompt("what is your name?");
+let userCity= prompt("from which  city are you from?");
+alert(`Hi,  ${userName} from ${userCity} , and welcome to javascript world .`);
+console.log(`Hi,  ${userName} from ${userCity} , and welcome to javascript world .`);
+ 
+
